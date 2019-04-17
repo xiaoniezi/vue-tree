ea6ae98e5e90e22c35d1f0ec81b16f8e708a8925
xiaoniezi <niexueyun@gmail.com>
2019-04-17 18:21:00 +0800

    2019/04/17 - vue-cli@3.x

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+/*
+ * @file 配置
+ */
+module.exports = {
+	// indexPath: "../index.html",
+	outputDir: "./dist/Output",
+	assetsDir: "static",
+	baseUrl: "/",
+	productionSourceMap: false,// 不开启map
+	devServer: {// 启动配置
+		port: 8088
+	},
+};
