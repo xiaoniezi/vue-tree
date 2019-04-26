bad69ffb63de4026fcb4fa1e116abf9a653b297a
xiaoniezi <niexueyun@gmail.com>
2019-04-26 11:04:25 +0800

    2019/04/26 - 删除.babelrc

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	// indexPath: "../index.html",
 	outputDir: "./dist/Output",
 	assetsDir: "static",
-	baseUrl: "/",
+	// baseUrl: "/",
 	productionSourceMap: false,// 不开启map
 	devServer: {// 启动配置
 		port: 8088
